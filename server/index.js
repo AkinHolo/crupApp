69723cd368d5c81467cc80ae61357351ae29ca60
hologutiworld <holoakinseloyin+github@gmail.com>
2023-04-11 14:20:21 +0100

    commited server setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import  dotenv from 'dotenv';
+
+
+// const express = require ('express');
+// const cors = require ('cors'); 
+// const bodyParser = require ('body-arser');
+// const mongoose = require ('mongoose');
+// const dotenv = require ('dotenv');
+
+
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.CONNECT_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>app.listen(PORT, ()=>console.log(`Server running on port http://localhost:${PORT}`)))
+.catch((error) => console.log(`${error} did not connect`));
